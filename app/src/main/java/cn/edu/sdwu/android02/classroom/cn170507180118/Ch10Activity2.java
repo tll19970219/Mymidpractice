908f56f56bd24fc8d3da05985d943bf3771eda90
tll19970219 <2397402275@qq.com>
2020-04-30 15:08:53 +0800

    2020-4-30

--- app/src/main/java/cn/edu/sdwu/android02/classroom/cn170507180118/Ch10Activity2.java
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/cn170507180118/Ch10Activity2.java
@@ -1,10 +1,13 @@
 package cn.edu.sdwu.android02.classroom.cn170507180118;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -52,7 +55,15 @@ public class Ch10Activity2 extends AppCompatActivity {
             if(resultCode==RESULT_OK){
                 //的带联系人信息(联系人的编号，lookup uri)
                 String content=data.getDataString();
-                Toast.makeText(this,content,Toast.LENGTH_SHORT).show();
+               Log.i(Ch10Activity2.class.toString(),data.getData().toString());
+                ContentResolver contentResolver=this.getContentResolver();
+                Cursor cursor=contentResolver.query(data.getData(),null,null,null,null);
+                while (cursor.moveToNext()){
+                    String dispName=cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                    Toast.makeText(this,dispName,Toast.LENGTH_SHORT).show();
+                }
+                cursor.close();
+
             }else{
                 Toast.makeText(this,"cancel",Toast.LENGTH_SHORT).show();
             }
