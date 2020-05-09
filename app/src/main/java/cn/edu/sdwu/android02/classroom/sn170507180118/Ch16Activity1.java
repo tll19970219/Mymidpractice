eae3b37224df23a0a6814d8f2b0a701630b68548
tll19970219 <2397402275@qq.com>
2020-05-09 19:30:27 +0800

    2020-5-9

--- app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180118/Ch16Activity1.java
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180118/Ch16Activity1.java
@@ -4,13 +4,20 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
+import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureFailure;
 import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.TotalCaptureResult;
+import android.media.Image;
+import android.media.ImageReader;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
 import android.support.annotation.IntDef;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -20,7 +27,11 @@ import android.util.Log;
 import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
 import java.util.Arrays;
 
 public class Ch16Activity1 extends AppCompatActivity {
@@ -31,6 +42,36 @@ private TextureView textureView;
     private CaptureRequest.Builder captureRequestBuilder;//请求的构造器
     private CaptureRequest previewRequest;
     private CameraCaptureSession cameraCaptureSession;
+    private ImageReader imageReader;//接受相机生成的静态图像
+   public void takephoto(View view){
+       //点击快门，生成静态图像
+       if(cameraDevice!=null){
+           //使用builder创建请求
+           try {
+               CaptureRequest.Builder builder=cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+               builder.addTarget(imageReader.getSurface());
+               //停止连续取景
+               cameraCaptureSession.stopRepeating();
+               //捕获静态图像
+               cameraCaptureSession.capture(builder.build(), new CameraCaptureSession.CaptureCallback() {
+                   @Override
+                   public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
+                     //捕获后，恢复连续取景
+                       try {
+                           session.setRepeatingRequest(previewRequest,null,null);
+                       } catch (Exception e) {
+                           Log.e(Ch16Activity1.class.toString(),e.toString());
+                       }
+                   }
+               },null);
+
+           }catch (Exception e){
+               Log.e(Ch16Activity1.class.toString(),e.toString());
+           }
+
+
+       }
+   }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,10 +99,40 @@ private TextureView textureView;
                     captureRequestBuilder=cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
                    captureRequestBuilder.addTarget(surface);//指定视频输出的位置
 
+                    imageReader=ImageReader.newInstance(1024,768, ImageFormat.JPEG,2);
+                    imageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
+                        @Override
+                        public void onImageAvailable(ImageReader imageReader) {
+                            //当照片数据可用时，激发该方法
+                            //获取捕获的照片数据
+                          Image image=imageReader.acquireNextImage();
+                            ByteBuffer buffer=image.getPlanes()[0].getBuffer();
+                            byte[] bytes=new byte[buffer.remaining()];
+                            buffer.get(bytes);
+                            //写文件
+                            File file=new File(Environment.getExternalStorageDirectory(),"abcd.jpg");
+                            FileOutputStream outputStream=null;
+                            try{
+                                outputStream=new FileOutputStream(file);
+                                outputStream.write(bytes);
+                                Toast.makeText(Ch16Activity1.this,"save:"+file,Toast.LENGTH_LONG).show();;
+                            }catch (Exception e){
+                             Log.e(Ch16Activity1.class.toString(),e.toString());
+                            }finally {
+                                try {
+                                    outputStream.flush();
+                                    outputStream.close();
+                                }catch (Exception e){
+                                    Log.e(Ch16Activity1.class.toString(),e.toString());
+                                }
+                            }
+                        }
+                    },null);
+
                     //创建一个相机捕捉会话
                     //参数1代表后续预览或拍照使用的组件
                     //参数2代表的是监听器，创建会话完成后执行的方法
-                    cameraDevice.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback() {
+                    cameraDevice.createCaptureSession(Arrays.asList(surface,imageReader.getSurface()), new CameraCaptureSession.StateCallback() {
                         @Override
                         public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
                             //会话创建完成后，我们在参数中得到会话的对象
@@ -93,7 +164,7 @@ private TextureView textureView;
             }
 
             @Override
-            public void onError(@NonNull CameraDevice cameraDevice, @IntDef(value = {CameraDevice.StateCallback.ERROR_CAMERA_IN_USE, CameraDevice.StateCallback.ERROR_MAX_CAMERAS_IN_USE, CameraDevice.StateCallback.ERROR_CAMERA_DISABLED, CameraDevice.StateCallback.ERROR_CAMERA_DEVICE, CameraDevice.StateCallback.ERROR_CAMERA_SERVICE}) int i) {
+            public void onError(@NonNull CameraDevice cameraDevice,  int i) {
 
             }
         };
